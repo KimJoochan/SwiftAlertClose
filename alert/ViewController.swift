2c50597bc763fa35738c949de73431a5c41a5ea6
dit06 <dit@imac-5.local>
2019-04-17 14:20:18 +0900

    ㄴㅇㄹㅇㄴㄹ

--- alert/ViewController.swift
+++ alert/ViewController.swift
@@ -16,23 +16,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func button(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Cammer", message: "Look at the me", preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Cammer", message: "Look at the me", preferredStyle: .actionSheet)
+        //alert는 경고창,actionSheet는 밑에서 올라오기
         let camAction = UIAlertAction(title: "Open Cammer", style: .default, handler: nil)
         let libAction = UIAlertAction(title: "사진 폴더 열기", style: .default, handler:nil)
             //self.view.backgroundColor = UIColor.yellow
         let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler:{(action:UIAlertAction) -> Void in self.view.backgroundColor=UIColor.yellow})
+        //후행 클로즈와 일반 클로즈 나옵니다.
        // let deleteAction = UIAlertAction(title: "Delete", style: .default, handler:{(action: UIAlertAction)-> Void in self.view.backgroundColor=UIColor.cyan})
-        let delete = UIAlertAction(title: "사진 폴더 열기", style: .default) {
+        let okay = UIAlertAction(title: "OKAY", style: .default){(action : UIAlertAction) ->Void in self.outlabel.text="Hello workd"}
+        let delete = UIAlertAction(title: "사진 폴더 취소", style: .destructive) {
             (action: UIAlertAction) -> Void in
             //self.view.backgroundColor = UIColor.yellow
             self.outlabel.text = "사진 폴더 열기 실행됨"
         };
-        let okay = UIAlertAction(title: "OKAY", style: .default){(action : UIAlertAction) ->Void in self.outlabel.text="Hello workd"}
+       
         alertController.addAction(camAction)
         alertController.addAction(libAction)
         alertController.addAction(cancelAction)
-        alertController.addAction(delete)
         alertController.addAction(okay)
+        alertController.addAction(delete)
         
         present(alertController, animated: true, completion: nil)
     }
